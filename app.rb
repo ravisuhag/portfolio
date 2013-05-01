5f05f0b8e0c213d8a1c9ea0742f6a87a2b0e2ec9
Ravi Suhag <suhag.ravi@gmail.com>
2013-05-02 01:53:24 +0530

    adding db

--- app.rb
+++ app.rb
@@ -5,6 +5,18 @@ require 'data_mapper'
 require 'erb'
 enable :sessions
 
+DataMapper.setup(:default, 'postgres://x:x@ec2-54-243-190-226.compute-1.amazonaws.com:5432/damsh7voso9tp3')
+
+class Messages 
+  include DataMapper::Resource  
+  property :id, Serial  
+  property :name, Text, :required => true
+  property :email_address, Text, :required => true
+  property :message, Text, :required => true    
+  property :created_at, DateTime  
+end
+DataMapper.finalize.auto_upgrade!
+
 get '/' do
 	erb :index
 end
@@ -20,3 +32,19 @@ end
 get '/connect' do
 	erb :connect
 end
+
+post '/connect' do  
+  n = Messages.new  
+  n.message = params[:message]
+  n.name = params[:name]
+  n.email_address = params[:email_address]  
+  n.created_at = Time.now  
+  n.save  
+  redirect '/connect'  
+end 
+get '/adminadminadmin' do  
+  @notes = Messages.all :order => :id.desc  
+  @title = 'All Messages'  
+  erb :admin 
+end 
+
