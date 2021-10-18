23c16758ae57fe76a84b4cc27ae3e15ca6f6b7b8
Ravi Suhag <suhag.ravi@gmail.com>
2021-10-17 20:37:59 -0700

    feat: add projects page

---
+++ pages/projects.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Header from '../components/header';
+import Videos from '../components/videos';
+
+export default function Bio() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Talks | Ravi Suhag</title>
+        <meta name="description" content="Ravi Suhag's personal homepage" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="main">
+        <h1>Talks</h1>
+        <p>
+          Browse through some of my past projects to see why I made it, what tools and technologies were used and where
+          I got the data from. Here, you can also find snapshots or, working examples and detailing the
+          different aspects of each project.
+        </p>
+      </main>
+    </div>
+  )
+}
