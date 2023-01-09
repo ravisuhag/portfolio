a043da6e529dc890022e3ea382251a997e35b530
Ravi Suhag <suhag.ravi@gmail.com>
2023-01-09 07:05:34 -0600

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,14 @@ import '../styles/globals.css'
 import '../styles/dark.css'
 import '../styles/prism.css'
 
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>)
 }
 
 export default MyApp
