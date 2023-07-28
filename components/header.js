d98f86a0090e5d24ca125a55533588b549acbd83
Ravi Suhag <suhag.ravi@gmail.com>
2023-07-28 18:23:51 -0500

    feat: add mobile nav

--- components/header.js
+++ components/header.js
@@ -1,18 +1,44 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import * as Popover from '@radix-ui/react-popover';
+
+const Menu = () => {
+  const router = useRouter();
+  return (
+    <div className="menu">
+      <Link href="/" className={router.pathname == "/" ? "active" : ""}>Bio.</Link>
+      <Link href="/work" className={router.pathname == "/work" ? "active" : ""}>Work.</Link>
+      <Link href="/posts" className={router.pathname == "/posts" ? "active" : ""}>Posts.</Link>
+      <Link href="/talks" className={router.pathname == "/talks" ? "active" : ""}>Talks.</Link>
+      <Link href="/contact" className={router.pathname == "/contact" ? "active" : ""}>Connect.</Link>
+    </div>
+  )
+}
 
 export default function Header() {
-  const router = useRouter();
+
   return (
     <div className="header">
       <Link href="/" className="header__logo">Ravi Suhag</Link>
-      <div className="navbar">
-        <Link href="/" className={router.pathname == "/" ? "active" : ""}>Bio.</Link>
-        <Link href="/work" className={router.pathname == "/work" ? "active" : ""}>Work.</Link>
-        <Link href="/posts" className={router.pathname == "/posts" ? "active" : ""}>Posts.</Link>
-        <Link href="/talks" className={router.pathname == "/talks" ? "active" : ""}>Talks.</Link>
-        <Link href="/contact" className={router.pathname == "/contact" ? "active" : ""}>Connect.</Link>
+      <div className='navbar'>
+        <Menu />
       </div>
+      <Popover.Root>
+        <Popover.Trigger asChild>
+          <HamburgerMenuIcon className='navbar__open' />
+        </Popover.Trigger>
+        <Popover.Anchor className='navbar__anchor' />
+        <Popover.Portal>
+          <Popover.Content className='navbar__mobile'>
+            <Popover.Close asChild>
+              <div className='navbar__close'><Cross2Icon /></div>
+            </Popover.Close>
+            <Menu />
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
     </div>
   )
 }
