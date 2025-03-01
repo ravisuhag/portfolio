3b3cffc62b41264b255ae8aea4efab3a3c6fcf4c
Ravi Suhag <suhag.ravi@gmail.com>
2025-03-01 07:23:09 +0530

    fix: path issues

--- app/posts/[id]/opengraph-image.js
+++ app/posts/[id]/opengraph-image.js
@@ -1,32 +1,32 @@
-// import { ImageResponse } from "next/og";
-// import { getPostData } from "@/lib/posts";
+import { ImageResponse } from "next/og";
+import { getPostData } from "@/lib/posts";
 
-// export default async function Image({ params }) {
-//   const { id } = await params;
-//   const post = await getPostData(id);
+export default async function Image({ params }) {
+  const { id } = await params;
+  const post = await getPostData(id);
 
-//   return new ImageResponse(
-//     (
-//       <div
-//         style={{
-//           fontSize: 40,
-//           color: "black",
-//           background: "white",
-//           width: "100%",
-//           height: "100%",
-//           padding: "50px 200px",
-//           textAlign: "center",
-//           justifyContent: "center",
-//           alignItems: "center",
-//         }}
-//       >
-//         <h1>{post.title}</h1>
-//         <p>{post.description}</p>
-//       </div>
-//     ),
-//     {
-//       width: 1200,
-//       height: 630,
-//     }
-//   );
-// }
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: "black",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>{post.title}</h1>
+        <p>{post.description}</p>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
